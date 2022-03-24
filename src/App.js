696bd3abb09d3ea850e140adf751bcba19c54077
stelleal <stellalleal@gmail.com>
2022-03-24 20:50:40 -0300

    Implementacao de estilos globais e google fonts

--- src/App.js
+++
@@ -1,7 +0,0 @@
-export function App() {
-  return (
-    <div className="App">
-      <h1>quero café</h1>
-    </div>
-  );
-}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+	return (
+		<div className="App">
+			<h1>quero café</h1>
+			<GlobalStyle />
+		</div>
+	);
+}
