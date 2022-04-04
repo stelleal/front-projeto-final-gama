7e58c04a9c9e955df700d256275b73372a5622d1
Leonardo <leonardokennedyalvesdelima@gmail.com>
2022-04-04 19:43:19 -0300

    Ajustes na Seção Um

--- src/components/SectionOne/index.jsx
+++ src/components/SectionOne/index.jsx
@@ -1,4 +1,5 @@
 import { Section } from './styles';
+import { Buttons } from '../Button';
 import coffeebeans from "../../../src/assets/images/coffee-beans.svg";
 
 export function SectionOne () {
@@ -14,11 +15,12 @@ export function SectionOne () {
                 <section className='info'>
                     <h1>Como funciona?</h1>
                     <ul>
-                        <li><h4>Escolha a região do grão</h4></li>
-                        <li><h4>Escolha o produto e o tipo de café</h4></li>
-                        <li><h4>Pague através do nosso WhatsApp</h4></li>
-                        <li><h4>Entregaremos no seu endereço</h4></li>
+                        <li><div className='steps'><h1>1</h1></div><h4>Escolha a região do grão</h4></li>
+                        <li><div className='steps'><h1>2</h1></div><h4>Escolha o produto e o tipo de café</h4></li>
+                        <li><div className='steps'><h1>3</h1></div><h4>Pague através do nosso WhatsApp</h4></li>
+                        <li><div className='steps'><h1>4</h1></div><h4>Entregaremos no seu endereço</h4></li>
                     </ul>
+                    <Buttons variante="calltoaction" texto="Quero escolher agora" />
                 </section>
         </Section>
     )
--- src/components/SectionOne/styles.js
+++ src/components/SectionOne/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 
 export const Section = styled.section`
+    margin: auto;
     height: auto;
     display: flex;
     justify-content: end;
@@ -39,22 +40,47 @@ export const Section = styled.section`
  }
 
  .info {
+     margin: 5rem 0;
      display: flex;
      flex-direction: column;
      background-color: rgba(255, 255, 255, 0.3);
 
+
+    .steps {
+        margin: 0 0 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 5.5rem;
+        height: 5.5rem;
+        background-color: var(--secondaryblue);
+        border-radius: 100%;
+
+        h1 {
+            margin: 0;
+            color: var(--textwhite);
+        }
+    }
+
      h1 {
-        margin-bottom: 3.5rem;
+        margin-bottom: 5rem;
         font-size: 2.75rem;
         color: var(--primaryorange);
      }
 
      ul {
+        margin-bottom: 5rem;
         display: flex;
+        justify-content: center;
         gap: 1.5rem;
 
         li {
+            
             list-style: none;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
             
             h4 {
                 display: flex;
