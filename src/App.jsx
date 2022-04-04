2d74476fc533c38018e361523966a1aae10e5aa3
camilapinheiro <camila.eug.pinheiro@gmail.com>
2022-04-04 19:53:15 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import { Navbar } from './components/Navbar';
 import { Banner } from './components/Banner';
 import { SectionOne } from './components/SectionOne';
 import { Buttons } from './components/Button';
-<<<<<<< HEAD
 import {Card} from './components/Cards'
-=======
->>>>>>> 2a7dc18674096d10f565368713c27610417a4ec5
 import { Footer } from './components/Footer';
 
 export function App() {
@@ -16,14 +13,9 @@ export function App() {
 			<Banner />
 			<GlobalStyle />
 			<SectionOne />
-<<<<<<< HEAD
 			<Card/>
 			<Buttons variante="calltoaction" texto="Saiba mais" />
 			<Buttons variante="secondarybutton" texto="Saiba mais" />
-=======
-			{/* <Buttons variante="calltoaction" texto="Saiba mais" />
-			<Buttons variante="secondarybutton" texto="Saiba mais" /> */}
->>>>>>> 2a7dc18674096d10f565368713c27610417a4ec5
 			<Footer />
 		</>
 	);
