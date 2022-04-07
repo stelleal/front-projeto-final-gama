592f4b890eaee636a4f7872042557ed54707b7c0
stelleal <stellalleal@gmail.com>
2022-04-07 09:09:16 -0300

    alterado class para className e body para <> (fragmento react) em Cards/index.jsx

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -2,15 +2,15 @@ import "./style.css";
 
 export function Card () {
   return ( 
-  <body> 
-    <div class="row">
-      <div class='Card'>
-        <img class="img-card" src="" />
+  <> 
+    <div className="row">
+      <div className='Card'>
+        <img className="img-card" src="" alt="" />
         <h2>Café beta 200g</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
         <p>Moido | Nordeste | Amargo</p>
-        <button class="but-card">Comprar</button>
+        <button className="but-card">Comprar</button>
       </div>
     </div>
-  </body> 
+  </> 
   )}
