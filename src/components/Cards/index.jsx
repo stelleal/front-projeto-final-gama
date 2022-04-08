b036204baf5d3e0c85f4124bc9a66a9a54f50289
camilapinheiro <camila.eug.pinheiro@gmail.com>
2022-04-07 22:10:33 -0300

    estilização dos cards, correção de erros e import de img

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,15 +1,38 @@
 import "./style.css";
+import imagem from "../../assets/images/coffee1.png";
+import { Buttons } from "../Button";
 
 export function Card () {
   return ( 
   <> 
     <div className="row">
-      <div className='Card'>
-        <img className="img-card" src="" alt="" />
+      <div className='card'>
+      <img className='img-card' src={imagem} alt="coffee" />
         <h2>Café beta 200g</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
         <p>Moido | Nordeste | Amargo</p>
-        <button className="but-card">Comprar</button>
+        <button className="but-card">COMPRAR</button>
+      </div>
+      <div className='card'>
+      <img className='img-card' src={imagem} alt="coffee" />
+        <h2>Café beta 200g</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
+        <p>Moido | Nordeste | Amargo</p>
+        <button className="but-card">COMPRAR</button>
+      </div>
+      <div className='card'>
+      <img className='img-card' src={imagem} alt="coffee" />
+        <h2>Café beta 200g</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
+        <p>Moido | Nordeste | Amargo</p>
+        <button className="but-card">COMPRAR</button>
+      </div>
+      <div className='card'>
+      <img className='img-card' src={imagem} alt="coffee" />
+        <h2>Café beta 200g</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
+        <p>Moido | Nordeste | Amargo</p>
+        <button className="but-card">COMPRAR</button>
       </div>
     </div>
   </> 
