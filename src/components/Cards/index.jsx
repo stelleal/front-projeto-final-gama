3a14c36c1fb1c71c0bcae124f0c7e3e6c95fed43
Leonardo <leonardokennedyalvesdelima@gmail.com>
2022-04-08 10:55:10 -0300

    Links comprar

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,6 +1,7 @@
 import "./style.css";
 import imagem from "../../assets/images/coffee1.png";
 import { Buttons } from "../Button";
+import { Link } from "react-router-dom";
 
 export function Card () {
   return ( 
@@ -11,28 +12,36 @@ export function Card () {
         <h3>Café beta 200g</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
         <p>Moido | Nordeste | Amargo</p>
-        <Buttons givenClass="primarybutton" text="Comprar" />
+        <Link to = "/product">
+          <Buttons givenClass="primarybutton" text="Comprar" />
+        </Link>
       </div>
       <div className='card'>
       <img className='img-card' src={imagem} alt="coffee" />
         <h3>Café beta 200g</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
         <p>Moido | Nordeste | Amargo</p>
-        <Buttons givenClass="primarybutton" text="Comprar" />
+        <Link to = "/product">
+          <Buttons givenClass="primarybutton" text="Comprar" />
+        </Link>
       </div>
       <div className='card'>
       <img className='img-card' src={imagem} alt="coffee" />
         <h3>Café beta 200g</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
         <p>Moido | Nordeste | Amargo</p>
-        <Buttons givenClass="primarybutton" text="Comprar" />
+        <Link to = "/product">
+          <Buttons givenClass="primarybutton" text="Comprar" />
+        </Link>
       </div>
       <div className='card'>
       <img className='img-card' src={imagem} alt="coffee" />
         <h3>Café beta 200g</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero culpa dolorem saepe minus. Tempora, tempore unde? Dolor, quibusdam saepe!</p>
         <p>Moido | Nordeste | Amargo</p>
-        <Buttons givenClass="primarybutton" text="Comprar" />
+        <Link to = "/product">
+          <Buttons givenClass="primarybutton" text="Comprar" />
+        </Link>
       </div>
     </div>
   </> 
